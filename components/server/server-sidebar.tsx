ccfe48e8b8693f7444850b3a222954c04c5c6aa8
Emeka-web-dev <chukwurex042@gmail.com>
2024-02-09 06:08:30 -0800

    server channel

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -8,6 +8,8 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerSearch } from "./server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -123,8 +125,21 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
         </div>
         <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
         {!!textChannels?.length && (
-          <div className="mb-2">
-            <ServerSection />
+          <div className="mb-2 mx-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            {textChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                server={server}
+                role={role}
+              />
+            ))}
           </div>
         )}
       </ScrollArea>
